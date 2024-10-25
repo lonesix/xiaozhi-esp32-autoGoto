518cdcb974c8898c9d99e6fe3cec079c84fc042a
lonesix <111976810+lonesix@users.noreply.github.com>
2024-10-25 10:28:56 +0800

    0.1 test finished:add AudioProcessor vad,and add ws send vad json

--- main/AudioProcessor.h
+++ main/AudioProcessor.h
@@ -20,13 +20,17 @@ public:
     void Stop();
     bool IsRunning();
     void OnOutput(std::function<void(std::vector<int16_t>&& data)> callback);
-
+    void OnVadStateChange(std::function<void(bool speaking)> callback);
+    void OnVadIsOpen(std::function<bool()> callback);
+    void OnVadWsSendFlag(std::function<void(bool vad_flag)> callback);
 private:
     EventGroupHandle_t event_group_ = nullptr;
     esp_afe_sr_data_t* afe_communication_data_ = nullptr;
     std::vector<int16_t> input_buffer_;
     std::function<void(std::vector<int16_t>&& data)> output_callback_;
-
+    std::function<void(bool speaking)> vad_state_callback_;
+    std::function<bool()> vad_isopen_callback_;
+    std::function<void(bool vad_flag)> vad_wssendflag_callback_;
     void AudioProcessorTask();
 };
 
